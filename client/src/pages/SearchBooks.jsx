2c8ecf48eb77d946565222d0e4e3e58c342d2e1f
AnnaT7459 <141446349+AnnaT7459@users.noreply.github.com>
2024-02-27 17:01:11 -0600

    added useMutation hook to exectute the SAVE_BOOK mutation in the handleSaveBook function, kept logic for saving book's ID

--- client/src/pages/SearchBooks.jsx
+++ client/src/pages/SearchBooks.jsx
@@ -21,6 +21,7 @@ const SearchBooks = () => {
   const [searchInput, setSearchInput] = useState('');
 
   // create state to hold saved bookId values
+  const [saveBook, { error }] = useMutation(SAVE_BOOK);
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
 
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
@@ -74,11 +75,11 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
+      const { data } = await saveBook({ varaiables: { bookData: {...bookToSave}}});
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+      // if (!response.ok) {
+      //   throw new Error('something went wrong!');
+      // }
 
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
