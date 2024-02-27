a51b2d5bc22c98c868665d3e2946cb57c571be7d
AnnaT7459 <141446349+AnnaT7459@users.noreply.github.com>
2024-02-26 19:18:45 -0600

    fixed mutations to login user

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -3,7 +3,7 @@ import { gql } from '@apollo/client';
 
 export const LOGIN_USER = gql`
   mutation loginUser($email: String!, $password: String!) {
-    login(email: $email, password: $password) {
+    loginUser(email: $email, password: $password) {
       token
       user {
         _id
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -37,7 +37,7 @@ type Query {
 }
 
 type Mutation {
-  login(email: String!, password: String!): Auth
+  loginUser(email: String!, password: String!): Auth
   addUser(username: String!, email: String!, password: String!): Auth
   saveBook(userId: ID!, book: BookInput!): User
   removeBook(userId: ID!, bookId: String!): User
